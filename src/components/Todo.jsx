b50803d7645e1229fbcf4e847bc1e771725f2a63
sachithrakalani <kalanisathyani@gmail.com>
2024-09-29 20:23:55 +0530

    Display all todos using component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,22 +1,28 @@
 import { useState } from "react";
+import TodoItem from "./TodoItem";
 
-export function Todo() {
-    const [todo , setTodo] = useState("");
-    const [todos , setTodos] = useState([]);
-    function handleSubmit(e){
-        e.preventDefault();
-        setTodos([...todos,todo]);
-        setTodo("");
-    }
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
   return (
     <div>
-      <form onSubmit = {handleSubmit}>
-        <input onChange = {(e)=> setTodo(e.target.value)} 
-        value = {todo} 
-        type = "text"
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+          type="text"
         />
-        <button type = "submit"> Add </button>
+        <button type="submit"> Add </button>
       </form>
+      {/* {todos} */}
+      {todos.map((item) => (
+        <TodoItem key={item} item={item} />
+      ))}
     </div>
   );
 }
