e35c71ed594a8a9bc688662c06409d2d03349e2c
sachithrakalani <kalanisathyani@gmail.com>
2024-10-02 13:13:31 +0530

    Style submit button using css module

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import styles from './form.module.css';
+import styles from "./form.module.css";
 
-export default function Form({todos={todos} ,setTodos={setTodos}}) {
+export default function Form({ todos = { todos }, setTodos = { setTodos } }) {
   const [todo, setTodo] = useState("");
 
   function handleSubmit(e) {
@@ -12,13 +12,19 @@ export default function Form({todos={todos} ,setTodos={setTodos}}) {
   return (
     <div>
       <form className={styles.todoForm} onSubmit={handleSubmit}>
-        <input className={styles.inputField}
-          onChange={(e) => setTodo(e.target.value)}
-          value={todo}
-          type="text"
-          placeholder="Enter Todo item..."
-        />
-        <button type="submit"> Add </button>
+        <div className={styles.inputContainer}>
+          <input
+            className={styles.inputField}
+            onChange={(e) => setTodo(e.target.value)}
+            value={todo}
+            type="text"
+            placeholder="Enter Todo item..."
+          />
+          <button className={styles.submitButton} type="submit">
+            {" "}
+            Add{" "}
+          </button>
+        </div>
       </form>
     </div>
   );
