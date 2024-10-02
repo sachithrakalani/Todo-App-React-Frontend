396da16ac73b7ad17c6fb3291a2472de690e4651
sachithrakalani <kalanisathyani@gmail.com>
2024-10-02 12:40:20 +0530

    Style input field using css module

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,10 +12,11 @@ export default function Form({todos={todos} ,setTodos={setTodos}}) {
   return (
     <div>
       <form className={styles.todoForm} onSubmit={handleSubmit}>
-        <input
+        <input className={styles.inputField}
           onChange={(e) => setTodo(e.target.value)}
           value={todo}
           type="text"
+          placeholder="Enter Todo item..."
         />
         <button type="submit"> Add </button>
       </form>
