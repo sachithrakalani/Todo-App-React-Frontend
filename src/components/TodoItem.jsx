dcf5f8e200fed41c9e2207cd734da62695df84a9
sachithrakalani <kalanisathyani@gmail.com>
2024-10-05 21:58:59 +0530

    Style Delete button using css module

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,11 @@
 import styles from "./todoitem.module.css";
 
 export default function TodoItem({ item }) {
-    return (
-      <div className={styles.itemcontainer}>
-        <input className={styles.todocheckbox} type="radio"/>
-        <span className={styles.todotextname}>{item}</span>
-        <span>
-          <button>x</button>
-        </span>
-      </div>
-    );
+  return (
+    <div className={styles.itemcontainer}>
+      <input className={styles.todocheckbox} type="radio" />
+      <span className={styles.todotextname}>{item}</span>
+      <span> <button className={styles.deleteButton}>x</button></span>
+    </div>
+  );
 }
