fb1d6ac0b664423bb78e627f034527b565ca2482
sachithrakalani <kalanisathyani@gmail.com>
2024-10-07 10:16:00 +0530

    Added handleDelete function to the delete button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,22 @@
 import styles from "./todoitem.module.css";
 
 export default function TodoItem({ item }) {
+  function handleDelete(item) {
+    console.log("Press Delete Button", item);
+  }
   return (
     <div className={styles.itemcontainer}>
       <input className={styles.todocheckbox} type="radio" />
       <span className={styles.todotextname}>{item}</span>
-      <span> <button className={styles.deleteButton}>x</button></span>
+      <span>
+        {" "}
+        <button
+          onClick={() => handleDelete(item)}
+          className={styles.deleteButton}
+        >
+          x
+        </button>
+      </span>
     </div>
   );
 }
