6b05dbc5c073ff8f881fcd0e81e476b96c5b305e
sachithrakalani <kalanisathyani@gmail.com>
2024-10-10 15:00:30 +0530

    Created todo item complete function

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,16 +1,80 @@
+// import { useState } from "react";
+// import styles from "./todoitem.module.css";
+
+// export default function TodoItem({ item, todos, setTodos }) {
+//   const [isChecked, setIsChecked] = useState(false);
+//   function handleDelete(item) {
+//     console.log("Press Delete Button", item);
+//     setTodos(todos.filter((todo) => todo !== item));
+//   }
+
+//   function handleClicked() {
+//     setIsChecked(!isChecked);
+//   }
+
+//   return (
+//     <div className={styles.itemcontainer}>
+//       <input
+//         className={styles.todocheckbox}
+//         type="radio"
+//         checked={isChecked}
+//         onClick={handleClicked}
+//       />
+//       <span
+//         className={`${styles.todotextname} ${
+//           isChecked ? styles.strikethrough : ""
+//         }`}
+//       >
+//         {item}
+//       </span>
+//       <span>
+//         {" "}
+//         <button
+//           onClick={() => handleDelete(item)}
+//           className={styles.deleteButton}
+//         >
+//           x
+//         </button>
+//       </span>
+//     </div>
+//   );
+// }
+
+
+import { useState } from "react";
 import styles from "./todoitem.module.css";
 
-export default function TodoItem({ item , todos , setTodos }) {
+export default function TodoItem({ item, date, todos, setTodos }) {
+  const [isChecked, setIsChecked] = useState(false);
+
   function handleDelete(item) {
     console.log("Press Delete Button", item);
-    setTodos(todos.filter((todo)=> todo !== item));
+    setTodos(todos.filter((todo) => todo !== item));
   }
+
+  function handleClicked() {
+    setIsChecked(!isChecked);
+  }
+
   return (
     <div className={styles.itemcontainer}>
-      <input className={styles.todocheckbox} type="radio" />
-      <span className={styles.todotextname}>{item}</span>
+      <input
+        className={styles.todocheckbox}
+        type="radio"
+        checked={isChecked}
+        onClick={handleClicked}
+      />
+      <span
+        className={`${styles.todotextname} ${
+          isChecked ? styles.strikethrough : ""
+        }`}
+      >
+        {item}
+      </span>
+      <span className={styles.tododate}>
+        {date && ` - Due: ${new Date(date).toLocaleDateString()}`}
+      </span>
       <span>
-        {" "}
         <button
           onClick={() => handleDelete(item)}
           className={styles.deleteButton}
